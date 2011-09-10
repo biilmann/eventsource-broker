4f12853f5485dc6983fe26cfca9cbe17fa5c450c
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2011-09-11 01:46:18 +0200

    Fix for bad channel in ruby test script

--- ruby/msg.rb
+++ ruby/msg.rb
@@ -2,6 +2,8 @@
 require 'json'
 require 'bunny'
 
+chan = ARGV.first || "test"
+
 client   = Bunny.new
 
 client.start
@@ -12,11 +14,11 @@ queue    = client.queue
 #puts "Enter messages (channell message)"
 loop do
   # msg = gets.strip
+  # _, chan, msg = *msg.match(/(\w+) (.+)/)
   msg = Time.now.to_s
-  _, chan, msg = *msg.match(/(\w+) (.+)/)
   exchange.publish({
     :channel => chan,
     :data    => msg
   }.to_json)
-  sleep 1000
+  sleep 1
 end
