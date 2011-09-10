12161d901b0f4980a75d0b9eea8851f5f81e76da
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2011-09-10 18:11:21 +0200

    Change msg.rb to just send the time every sec

--- ruby/msg.rb
+++ ruby/msg.rb
@@ -9,12 +9,14 @@ client.start
 exchange = client.exchange("eventsource.fanout", :type => :fanout)
 queue    = client.queue
 
-puts "Enter messages (channell message)"
+#puts "Enter messages (channell message)"
 loop do
-  msg = gets.strip
+  # msg = gets.strip
+  msg = Time.now.to_s
   _, chan, msg = *msg.match(/(\w+) (.+)/)
   exchange.publish({
     :channel => chan,
     :data    => msg
   }.to_json)
+  sleep 1000
 end
